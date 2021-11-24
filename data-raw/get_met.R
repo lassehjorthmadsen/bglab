e946af0589bc76303f9c3733b63972e68a6afb30
Lasse Hjorth Madsen <lassehjorthmadsen@gmail.com>
2021-11-24 22:00:53 +0100

    fiddle with gramEvol package

--- data-raw/get_met.R
+++ data-raw/get_met.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(readr)
+library(gramEvol)
 theme_set(theme_minimal())
 
 met_file <- "c:\\Program Files (x86)\\eXtreme Gammon 2\\MET\\Kazaross XG2.met"
@@ -14,3 +15,33 @@ met %>%
   ggplot(aes(x = p1, y = value, color = factor(p2))) +
   geom_line() +
   geom_point(size = 0.75)
+
+
+# Symbolic reg
+
+p1 <- met$p1
+p2 <- met$p2
+y  <- met$value
+
+ruleDef <- list(expr = grule(op(expr, expr), func(expr), var),
+                func = grule(sin, cos, log, sqrt),
+                op = grule('+', '-', '*', '/'),
+                var = grule(p1, p2))
+
+grammarDef <- CreateGrammar(ruleDef)
+grammarDef
+
+SymRegFitFunc <- function(expr) {
+  result <- eval(expr)
+  if (any(is.nan(result)))
+    return(Inf)
+  return (mean(log(1 + abs(y - result))))
+}
+
+set.seed(314)
+ge <- GrammaticalEvolution(grammarDef, SymRegFitFunc, terminationCost = 0.1, iterations = 250, max.depth = 3)
+ge
+
+best_expr <- ge$best$expression
+
+data.frame(p1, p2, me = y, ge = eval(best_expr))
