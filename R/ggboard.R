cac2d7b51a2b556ee9888797b45bf7cb8bf5fb80
Lasse Hjorth Madsen <lassehjorthmadsen@gmail.com>
2023-04-16 22:53:25 +0200

    more work on ggboard()

--- R/ggboard.R
+++ R/ggboard.R
@@ -4,21 +4,32 @@
 #'
 #' @return ggplot object
 #'
-#' @example
+#' @examples
 #' ggboard("XGID=-b----E-C---eE---c-e----B-:0:0:1:64:0:0:0:7:10")
+#' ggboard("XGID=-a-B--E-B-a-dDB--b-bcb----:1:1:-1:63:0:0:0:3:8")
 #'
 #' @importFrom ggforce geom_circle
 #'
 #' @export
 ggboard <- function(xgid) {
 
-  position <- empty_board() + show_cube() + show_points()
+  position <- ggplot() +
+    show_points() +
+    show_cube(xgid) +
+    show_points() +
+    show_bar() +
+    show_border() +
+    #show_numbers() +
+    show_checkers(xgid) +
+    coord_fixed() +
+    ggplot2::scale_fill_manual(values = c("black", "darkgrey", "white", "lightgrey")) +
+    ggplot2::theme_void()
+
   return(position)
 }
 
 
-empty_board <- function() {
-
+show_points <- function() {
   x <- 0:12 %>% purrr::map(~ rep(0:2/26, 2) + .x * 2/26) %>% unlist()
   y <- rep(c(0, 5/11, 0, 1, 6/11, 1), 13)
   fill <- rep(c(rep(T, 3), rep(F, 3), rep(F, 3), rep(T, 3)), 3)
@@ -27,36 +38,41 @@ empty_board <- function() {
 
   triangles <- dplyr::tibble(x = x, y = y, fill = fill, group = group)
 
-  board <- ggplot2::ggplot(data = NULL) +
-    ggplot2::geom_polygon(data = triangles,
+  ggplot2::geom_polygon(data = triangles,
                           ggplot2::aes(x = x, y = y, fill = fill, group = group),
-                 show.legend = F, color = "darkgrey") +
-    ggplot2::geom_rect(ggplot2::aes(xmin = 0, xmax = 1, ymin = 0, ymax = 1),
-              fill = NA, colour = "darkgrey", size = 0.5) +
-    ggplot2::geom_rect(ggplot2::aes(xmin = 6/13, xmax = 7/13, ymin = 0, ymax = 1),
-              fill = "white", colour = "darkgrey", size = 0.5, inherit.aes = F) +
-    ggplot2::scale_fill_manual(values = c("darkgrey", "white")) +
-    ggplot2::theme_minimal()
-
-  return(board)
+                 show.legend = F, color = "darkgrey")
 }
 
 
-show_cube <- function(cube = 2, cube_position = -1) {
+show_border <- function() {
+  ggplot2::geom_rect(ggplot2::aes(xmin = 0, xmax = 1, ymin = 0, ymax = 1),
+            fill = NA, colour = "darkgrey", size = 0.5)
+}
+
+
+show_bar <- function() {
+  ggplot2::geom_rect(ggplot2::aes(xmin = 6/13, xmax = 7/13, ymin = 0, ymax = 1),
+            fill = "white", colour = "darkgrey", size = 0.5, inherit.aes = F)
+}
+
+
+show_cube <- function(xgid) {
+
+  cube = substr(xgid, 33, 33) %>% as.numeric()
+  cube_position = substr(xgid, 35, 35) %>% as.numeric()
 
   cube_value = 2^cube
   if (cube_value == 1) cube_value <-64
+
   y_position <- -0.5 * cube_position + 0.5
 
-  cube <- ggplot2::geom_label(ggplot2::aes(x = -0.06, y = y_position, label = cube_value),
-                     size = 4,  color = "black", label.padding = ggplot2::unit(1.5, "mm"),
-                     label.size = 0.5, label.r = ggplot2::unit(0.5, "mm"))
-
-  return(cube)
+  ggplot2::geom_label(ggplot2::aes(x = -0.06, y = y_position, label = cube_value),
+                      size = 4,  color = "black", label.padding = ggplot2::unit(2, "mm"),
+                      label.size = 0.5, label.r = ggplot2::unit(0.75, "mm"))
 }
 
 
-show_points <- function(bearoff = "right") {
+show_numbers <- function(bearoff = "right") {
   points <- ggplot2::scale_x_continuous(breaks = seq(1/26, 25/26, 2/26),
                                labels = c(12, 11, 10, 9, 8, 7, "", 6, 5, 4, 3, 2, 1 ))
   return(points)
@@ -74,15 +90,15 @@ show_checkers <-  function(xgid, bearoff = "right", player_color = "black") {
 
     # x-coordinates for each point
     x_coord <- rep(NULL, 26)
-    x_coord[2:7] <- ((2:7) - 2) * 2/26
-    x_coord[8:13] <- ((8:13) - 1) * 2/26
-    x_coord[14:19] <- ((13:8) - 1) * 2/26
-    x_coord[20:25] <- ((7:2) - 2) * 2/26
-    x_coord[c(1,26)] <- 12/26
+    x_coord[2:7] <- ((2:7) - 2) * 2/26 + 1/26
+    x_coord[8:13] <- ((8:13) - 1) * 2/26 + 1/26
+    x_coord[14:19] <- ((13:8) - 1) * 2/26 + 1/26
+    x_coord[20:25] <- ((7:2) - 2) * 2/26 + 1/26
+    x_coord[c(1,26)] <- 12/26 + 1/26
 
     # y-coordinates for bottom and top points
-    y_coord_bottom <- seq(0, 4/11, 1/11)
-    y_coord_top <- seq(10/11, 6/11, -1/11)
+    y_coord_bottom <- seq(1/22, 9/22, 2/22)
+    y_coord_top <- seq(21/22, 13/22, -2/22)
 
     # Loop over all 26 points (#1 is our bar, #26 is opp's bar)
     for (i in seq(1, 26)) {
@@ -110,14 +126,8 @@ show_checkers <-  function(xgid, bearoff = "right", player_color = "black") {
     }
 
     df <- tibble(x = x, y = y, player = player)
-    return(df)
+
+    ggforce::geom_circle(mapping = ggplot2::aes(x0 = x, y0 = y, fill = player, r = 1/28),
+                         show.legend = F, inherit.aes = F, data = df)
+
 }
-
-library(tidyverse)
-library(ggforce)
-
-df <- show_checkers(xgid = xgid)
-
-df %>% ggplot() +
-  geom_circle(aes(x0 = x, y0 = y, fill = player, r = 1/22), show.legend = F) +
-  theme_void()
