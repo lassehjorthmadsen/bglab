1f8698da333e229ba6a64523b503d149b8c9ab7a
LMDN <LMDN@novonordisk.com>
2023-04-17 16:26:46 +0200

    fixed checker location

--- R/ggboard.R
+++ R/ggboard.R
@@ -11,7 +11,7 @@
 #' @importFrom ggforce geom_circle
 #'
 #' @export
-ggboard <- function(xgid) {
+ggboard <- function(xgid, bearoff = "right") {
 
   position <- ggplot() +
     show_points() +
@@ -19,7 +19,7 @@ ggboard <- function(xgid) {
     show_points() +
     show_bar() +
     show_border() +
-    #show_numbers() +
+    show_numbers(bearoff) +
     show_checkers(xgid) +
     coord_fixed() +
     ggplot2::scale_fill_manual(values = c("black", "darkgrey", "white", "lightgrey")) +
@@ -30,8 +30,10 @@ ggboard <- function(xgid) {
 
 
 show_points <- function() {
+  ratio <- 11/13 # Board is 11 checkers high, 13 checkers wide. Move to global env?
+
   x <- 0:12 %>% purrr::map(~ rep(0:2/26, 2) + .x * 2/26) %>% unlist()
-  y <- rep(c(0, 5/11, 0, 1, 6/11, 1), 13)
+  y <- rep(c(0, 5/11, 0, 1, 6/11, 1), 13) * ratio
   fill <- rep(c(rep(T, 3), rep(F, 3), rep(F, 3), rep(T, 3)), 3)
   fill <- c(fill, rep(F, 6), fill)
   group <- 1:26 %>% purrr::map(rep, 3) %>% unlist()
@@ -45,13 +47,16 @@ show_points <- function() {
 
 
 show_border <- function() {
-  ggplot2::geom_rect(ggplot2::aes(xmin = 0, xmax = 1, ymin = 0, ymax = 1),
+  ratio <- 11/13 # Board is 11 checkers high, 13 checkers wide. Move to global env?
+
+  ggplot2::geom_rect(ggplot2::aes(xmin = 0, xmax = 1, ymin = 0, ymax = ratio),
             fill = NA, colour = "darkgrey", size = 0.5)
 }
 
 
 show_bar <- function() {
-  ggplot2::geom_rect(ggplot2::aes(xmin = 6/13, xmax = 7/13, ymin = 0, ymax = 1),
+  ratio <- 11/13 # Board is 11 checkers high, 13 checkers wide. Move to global env?
+  ggplot2::geom_rect(ggplot2::aes(xmin = 6/13, xmax = 7/13, ymin = 0, ymax = ratio),
             fill = "white", colour = "darkgrey", size = 0.5, inherit.aes = F)
 }
 
@@ -73,61 +78,75 @@ show_cube <- function(xgid) {
 
 
 show_numbers <- function(bearoff = "right") {
-  points <- ggplot2::scale_x_continuous(breaks = seq(1/26, 25/26, 2/26),
-                               labels = c(12, 11, 10, 9, 8, 7, "", 6, 5, 4, 3, 2, 1 ))
-  return(points)
+  ratio <- 11/13 # Board is 11 checkers high, 13 checkers wide. Move to global env?
+
+  x <- rep(0:12  * 2/26 + 1/26, 2)
+  y <- c(rep(-0.02, 13), rep(ratio + 0.03, 13))
+
+  if (bearoff == "right") {
+    label <- as.character(c(1:6, NA, 7:12, 24:19, NA, 18:13))
+  } else if  (bearoff == "left") {
+    label = as.character(c(12:7, NA, 6:1, 13:18, NA, 19:24))
+  } else {
+    stop("bearoff parameter must be either 'right' or 'left'")
+  }
+
+  df = tibble(x, y, label)
+
+  ggplot2::geom_text(data = df, mapping = aes(x = x, y = y, label = label), color = "black", size = 3)
+
 }
 
 
 show_checkers <-  function(xgid, bearoff = "right", player_color = "black") {
+  if (nchar(xgid) != 51) stop("XGID does not contain exactly 51 characters")
 
-    if (nchar(xgid) != 51) stop("XGID does not contain exactly 51 characters")
+  ratio <- 11/13 # Board is 11 checkers high, 13 checkers wide. Move to global env?
 
-    # Init vars to populate
-    x <- NULL
-    y <- NULL
-    player <- NULL
+  # Init vars to populate
+  x <- NULL
+  y <- NULL
+  player <- NULL
 
-    # x-coordinates for each point
-    x_coord <- rep(NULL, 26)
-    x_coord[2:7] <- ((2:7) - 2) * 2/26 + 1/26
-    x_coord[8:13] <- ((8:13) - 1) * 2/26 + 1/26
-    x_coord[14:19] <- ((13:8) - 1) * 2/26 + 1/26
-    x_coord[20:25] <- ((7:2) - 2) * 2/26 + 1/26
-    x_coord[c(1,26)] <- 12/26 + 1/26
+  # x-coordinates for each point
+  x_coord <- rep(NULL, 26)
+  x_coord[2:7] <- ((2:7) - 2) * 2/26 + 1/26
+  x_coord[8:13] <- ((8:13) - 1) * 2/26 + 1/26
+  x_coord[14:19] <- ((13:8) - 1) * 2/26 + 1/26
+  x_coord[20:25] <- ((7:2) - 2) * 2/26 + 1/26
+  x_coord[c(1,26)] <- 12/26 + 1/26
 
-    # y-coordinates for bottom and top points
-    y_coord_bottom <- seq(1/22, 9/22, 2/22)
-    y_coord_top <- seq(21/22, 13/22, -2/22)
+  # y-coordinates for bottom and top points
+  y_coord_bottom <- seq(1/22, 9/22, 2/22) * ratio
+  y_coord_top <- seq(21/22, 13/22, -2/22) * ratio
 
-    # Loop over all 26 points (#1 is our bar, #26 is opp's bar)
-    for (i in seq(1, 26)) {
-      entry <- substr(xgid, i + 5, i + 5)
+  # Loop over all 26 points (#1 is our bar, #26 is opp's bar)
+  for (i in seq(1, 26)) {
+    entry <- substr(xgid, i + 5, i + 5)
 
-      if (entry != "-") {
-        player_checkers <- match(entry, LETTERS)
-        opponent_checkers <- match(entry, letters)
-        no_checkers <- coalesce(player_checkers, opponent_checkers)
+    if (entry != "-") {
+      player_checkers <- match(entry, LETTERS)
+      opponent_checkers <- match(entry, letters)
+      no_checkers <- coalesce(player_checkers, opponent_checkers)
 
-        if (is.na(no_checkers)) stop(paste("Illegal character found in XGID string: ", entry))
+      if (is.na(no_checkers)) stop(paste("Illegal character found in XGID string: ", entry))
 
-        x_point <- rep(x_coord[i], no_checkers)
+      x_point <- rep(x_coord[i], no_checkers)
 
-        y_point <- case_when(i < 14 ~ y_coord_bottom[1:no_checkers],
-                             i > 13 ~ y_coord_top[1:no_checkers])
+      y_point <- case_when(i < 14 ~ y_coord_bottom[1:no_checkers],
+                           i > 13 ~ y_coord_top[1:no_checkers])
 
-        p_point <-  rep(if_else(is.na(player_checkers), "top", "bottom"), no_checkers)
+      p_point <-  rep(if_else(is.na(player_checkers), "top", "bottom"), no_checkers)
 
 
-        x <-  c(x, x_point)
-        y <-  c(y, y_point)
-        player <- c(player, p_point)
-        }
-    }
+      x <-  c(x, x_point)
+      y <-  c(y, y_point)
+      player <- c(player, p_point)
+      }
+  }
 
-    df <- tibble(x = x, y = y, player = player)
+  df <- tibble(x = x, y = y, player = player)
 
-    ggforce::geom_circle(mapping = ggplot2::aes(x0 = x, y0 = y, fill = player, r = 1/28),
-                         show.legend = F, inherit.aes = F, data = df)
-
-}
+  ggforce::geom_circle(mapping = ggplot2::aes(x0 = x, y0 = y, fill = player, r = 1/26),
+                       show.legend = F, inherit.aes = F, data = df)
+  }
